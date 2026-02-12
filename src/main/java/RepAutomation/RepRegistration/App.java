3ed8b859c7378d37b0099e71b0f608e30b95238b
Shishir19999 <Shishirsharma199@gmail.com>
2026-02-12 16:25:13 +0545

    added gitignore, repo setup
    rep registration workflow

---
+++ src/main/java/RepAutomation/RepRegistration/App.java
@@ -0,0 +1,13 @@
+package RepAutomation.RepRegistration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
